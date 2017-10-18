f457d4a7c1fe11786d106c47decdbf71e1971f65
RoninYeti <RoninYetcen@live.com>
2017-10-18 08:57:28 -0400

    Made Minor Changes

---
+++ Assets/Resources/Scripts/SoulScripts/PlanetParticle.cs
@@ -0,0 +1,15 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace solmates {
+    public class PlanetParticle : MonoBehaviour {
+
+        private void SoulHit(Collider col) {
+            if (col.CompareTag("PureSoul")) {
+                transform.GetChild(0).gameObject.SetActive(true);
+                transform.GetChild(0).gameObject.SetActive(false);
+            }
+        }
+    }
+}*/
